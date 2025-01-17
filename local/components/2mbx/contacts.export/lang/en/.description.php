0cea2a3b54ebdfe1698862dc3eb0c01557ad012a
Oleg Matasov <matasov.pro@gmail.com>
2025-01-17 23:56:10 +0300

    Test task to simple export CRM contacts

---
+++ local/components/2mbx/contacts.export/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['EXP_CMP_NAME'] = 'Export contacts to csv';
+$MESS['EXP_CMP_DESCRIPTION'] = 'Simple component to export contacts to csv';
+$MESS['EXP_CMP_DESCRIPTION_GROUP'] = '2MBX';
+$MESS['EXP_CMP_DESCRIPTION_DIR'] = 'Contacts export';
---
+++ local/components/2mbx/contacts.export/lang/en/class.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['EXP_CMP_CRM_MODULE_NOT_INSTALLED'] = "Component error! Module 'crm' not installed or activated. Please activate module and try again.";
+$MESS['EXP_CMP_CRM_FACTORY_ERROR'] = "Component error! Error while trying to access to CRM Contacts. Please check your rights and try again.";
